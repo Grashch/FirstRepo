5a432d0d2293a67fcd29d44b9b833684349b23b5
ASGrashchenko <asgrashchenko@bk.ru>
2025-04-10 21:17:33 +0300

    Добавил .gitignore

---
+++ Learn.py
@@ -0,0 +1,14 @@
+def log_method_call(func):
+    def wrapper(self, *args, **kwargs):
+        print(f"Вызов метода {func.__name__}")
+        return func(self, *args, **kwargs)
+
+    return wrapper
+
+class MyClass:
+    @log_method_call
+    def say_hello(self):
+        print("Hello from MyClass!")
+
+obj = MyClass()
+obj.say_hello()
